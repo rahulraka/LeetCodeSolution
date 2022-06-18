581bc0bd215ea154a135b747b5497ccd12e3d4c5
Rahul Kumar <65751330+rahulraka@users.noreply.github.com>
2022-06-18 17:30:39 +0530

    Added solution - LeetHub

---
+++ Chocolate Distribution Problem - GFG/chocolate-distribution-problem.js
@@ -0,0 +1,81 @@
+// { Driver Code Starts
+//Initial Template for javascript
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function printList(res,n){
+    let s="";
+    for(let i=0;i<n;i++){
+        s+=res[i];
+        s+=" ";
+    }
+    console.log(s);
+}
+
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for(;i<t;i++)
+    {
+       
+        let n = parseInt(readLine());
+        let arr = new Array(n);
+        let input_ar1 = readLine().split(' ').map(x=>parseInt(x));
+        let m = parseInt(readLine());
+        for(let i=0;i<n;i++){
+            arr[i] = input_ar1[i];
+        }
+        let obj = new Solution();
+        let res = obj.findMinDiff(arr, n, m);
+        console.log(res);
+        
+    }
+}// } Driver Code Ends
+
+// } Driver Code Ends
+
+
+//User function Template for javascript
+
+/**
+ * @param {number[]} arr
+ * @param {number} n
+ * @param {number} m
+ * @returns {number}
+*/
+
+class Solution{
+    findMinDiff(arr,n,m){
+        //code here
+        arr.sort((a,b)=>{return a-b});
+        let min=Infinity;
+        for(let i=0;i+m-1<n;i++){
+            let diff=Math.abs(arr[i]-arr[i+m-1])
+        min=Math.min(min,diff)    
+        }
+        return min
+    }
+}
